e3ed6b9aba31ae6ec15382d4be1e1f3d88fdd6fb
knolegsb <seon2you@hotmail.com>
2016-07-09 00:37:26 -0700

    another controller

---
+++ Mvc5_ImageGallery/Controllers/ImageGalleryController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5_ImageGallery.Controllers
+{
+    public class ImageGalleryController : Controller
+    {
+        // GET: ImageGallery
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            List<ImageGallery> all = new List<ImageGallery>();
+            using (ImageGalleryContext context = new ImageGalleryContext())
+            {
+                all = context.ImageGalleries.ToList();
+            }
+
+            return View(all);
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(ImageGallery image)
+        {
+            if (image.File.ContentLength > (2 * 1024 * 1024))
+            {
+                ModelState.AddModelError("CustomError", "File size must be less than 2 MB");
+                return View(image);
+            }
+            if (!(image.File.ContentType == "image/jpeg" || image.File.ContentType == "image/png"))
+            {
+                ModelState.AddModelError("CustomError", "File type allowed : jpg and png");
+                return View();
+            }
+            image.FileName = image.File.FileName;
+            image.ImageSize = image.File.ContentLength;
+
+            byte[] data = new byte[image.File.ContentLength];
+            image.File.InputStream.Read(data, 0, image.File.ContentLength);
+
+            image.ImageData = data;
+            using (ImageGalleryContext context = new ImageGalleryContext())
+            {
+                context.ImageGalleries.Add(image);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Gallery");
+        }
+    }
+}
---
+++ Mvc5_ImageGallery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5_ImageGallery.Startup))]
+namespace Mvc5_ImageGallery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
